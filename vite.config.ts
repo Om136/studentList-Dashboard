75328b505fade2992be973aee529f4ad174f465b
Om136 <202251154@iiitvadodara.ac.in>
2025-05-08 19:55:20 +0530

    fix: update Vercel configuration to set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Set base to root for Vercel deployment
+  base: '/student-list-dashboard/',
   server: {
     host: true,
     port: 3000,
